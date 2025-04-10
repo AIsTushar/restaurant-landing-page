a4109c7bba91546b724eedc90c505feb154e1587
Azizul Islam <azizultushar98@gmail.com>
2025-04-10 23:36:04 +0600

    Refactor Hero component: move to Header directory and update import paths

--- src/app/page.js
+++ src/app/page.js
@@ -2,14 +2,14 @@ import About from "@/components/About/About";
 import BookNow from "@/components/BookNow/BookNow";
 import Footer from "@/components/Footer/Footer";
 import Header from "@/components/Header/Header";
-import Hero from "@/components/Hero/Hero";
+import Hero from "@/components/Header/Hero/Hero";
 import Popular from "@/components/Popular/Popular";
 import Testimonials from "@/components/Testimonials/Testimonials";
 
 export default function Home() {
   return (
     <main>
-      <div className="bg-[url('/Rectangle.png'),linear-gradient(123.4deg,_#bd1f17_40.8%,_#a61d13_76.4%,_#8e1b0f_120.69%)] bg-cover bg-center bg-no-repeat">
+      <div className="sticky top-0 bg-[url('/Rectangle.png'),linear-gradient(123.4deg,_#bd1f17_40.8%,_#a61d13_76.4%,_#8e1b0f_120.69%)] bg-cover bg-center bg-no-repeat">
         <Header />
         <Hero />
       </div>
